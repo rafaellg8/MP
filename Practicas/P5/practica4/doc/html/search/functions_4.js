e6d7427f0a2358dc49434184f9987d7ae5ed66ee
rafaellg8 <rafaellg8@corre.ugr.es>
2016-04-20 20:15:59 +0200

    all

---
+++ Practicas/P5/practica4/doc/html/search/functions_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['get',['get',['../classImagen.html#ab8a0d7d44f6d22678ed7fea1e9617e25',1,'Imagen']]],
+  ['getpos',['getPos',['../classImagen.html#a903bfa18bddaed521557d73d483b2087',1,'Imagen']]]
+];
