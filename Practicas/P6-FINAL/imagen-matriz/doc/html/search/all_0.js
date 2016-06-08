59ecfcf5283d04080037ba5a15cbba5b12192c36
rafaellg8 <rafaellg8@correo.ugr.es>
2016-06-08 18:26:44 +0200

    subo practica final

--- Practicas/P6-FINAL/imagen-matriz/doc/html/search/all_0.js
+++ Practicas/P6-FINAL/imagen-matriz/doc/html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['aarteascii',['aArteASCII',['../classImagen.html#a0b32aeab505b6efe14e6680fa2eb067b',1,'Imagen::aArteASCII(const char grises[], char **arteASCII, int maxlong)'],['../classImagen.html#adbb66160513ece2f58ed6c9f9fcb9794',1,'Imagen::aArteASCII(const char grises[], char **arteASCII, int maxlong, int cardinal)']]]
+  ['aarteascii',['aArteASCII',['../classImagen.html#adbb66160513ece2f58ed6c9f9fcb9794',1,'Imagen::aArteASCII(const char grises[], char **arteASCII, int maxlong, int cardinal)'],['../classImagen.html#a0b32aeab505b6efe14e6680fa2eb067b',1,'Imagen::aArteASCII(const char grises[], char **arteASCII, int maxlong)']]]
 ];
